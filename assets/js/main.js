93550bd121b94550e1d7647fb8df329c21269caf
Alecksandrew <alecksandrewcontato@gmail.com>
2025-09-04 15:46:12 -0300

    Lógica de paginação finalizada
    - Adicionada paginação com reticências se houver
    muitos resultados.
    - Paginação desaparece ao retornar resultados que
    podem ser mostrado em no máximo 1 página
    - Adicionado aria label e current para melhorar
    acessibilidade
    - CSS seguindo metodologia BEM

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,29 @@
 "use strict";
 
-async function fetchBasicPokemonData(searchData) {
+//Estado global
+const paginationState = {
+  currentPage: 1,
+  itemsPerPage: 18,
+  totalItems: 0,
+};
+
+const paginationContainer = document.querySelector(".pagination");
+
+//===================================LÓGICA DE BUSCA DE DADOS=============================================================
+async function fetchBasicPokemonData() {
   try {
+    const offset =
+      (paginationState.currentPage - 1) * paginationState.itemsPerPage;
+
     const response = await fetch(
-      `https://pokeapi.co/api/v2/pokemon/${
-        searchData == null || searchData == "" ? "" : searchData
-      }`
+      `https://pokeapi.co/api/v2/pokemon?limit=${paginationState.itemsPerPage}&offset=${offset}`
     );
-    console.log("FETCH BASIC DATA" + response);
     if (!response.ok) {
       throw new Error(response.statusText);
     }
 
     const pokemonData = await response.json();
-    console.log(pokemonData.results);
+    paginationState.totalItems = pokemonData.count;
     return pokemonData.results;
   } catch (error) {
     console.error(error); // -> Vou tentar fazer um card mostrando o tipo de erro que ocorrer
@@ -40,6 +50,7 @@ async function fetchComprehensivePokemonData(pokemonName) {
   }
 }
 
+//===================================LOGICA DE RENDERIZAÇÃO============================================================
 function createCard(type, id, imageUrl, title) {
   const li = document.createElement("li");
   li.className = "cards-container__card card";
@@ -77,21 +88,23 @@ function renderCardsWithData(data) {
 
   if (Array.isArray(data)) {
     data.forEach((pokemonData) => {
-      const {id, type, img, name} = validateData(pokemonData);
+      const { id, type, img, name } = validateData(pokemonData);
 
       const newCard = createCard(type, id, img, name);
       cardsContainer.appendChild(newCard);
     });
   } else {
-    const {id, type, img, name} = validateData(data);
+    const { id, type, img, name } = validateData(data);
 
     const newCard = createCard(type, id, img, name);
     cardsContainer.appendChild(newCard);
   }
 }
 
+
+//===================================LOGICA PRINCIPAL=============================================================
 function handleFormSubmit() {
-  const searchForm = document.getElementsByClassName("search-form")[0]; //Estou utilizando className ao inves de atribuir um ID                                                                //para reaproveitar class já criada e porque o projeto é pequeno;
+  const searchForm = document.getElementsByClassName("search-form")[0]; //Estou utilizando className ao inves de atribuir um ID para reaproveitar class já criada e porque o projeto é pequeno;
   const searchInput = document.getElementsByClassName("search-form__input")[0];
 
   searchForm.addEventListener("submit", async (e) => {
@@ -105,6 +118,7 @@ function handleFormSubmit() {
         searchText
       );
       renderCardsWithData(comprehensiveDataFromAPI);
+      paginationContainer.innerHTML = "";
       searchForm.reset();
     }
   });
@@ -122,13 +136,156 @@ async function handleInitialPageFetch() {
   );
 
   renderCardsWithData(comprehensiveDataFromAPI);
+
+  setupPagination(
+    paginationState.totalItems,
+    paginationState.itemsPerPage,
+    paginationState.currentPage
+  );
 }
 
-//===================================EXECUÇÃO=========================================================================================
 
+//=========================LOGICA DE PAGINAÇÃO============================================================
+
+
+
+function setupPagination(totalItems, itemsPerPage, currentPage) {
+  paginationContainer.innerHTML = "";
+
+  const totalPages = Math.ceil(totalItems / itemsPerPage);
+
+  if (totalPages <= 1) return;
+
+  //Array com a lógica de paginação
+  const pages = createPaginationLogic(totalPages, currentPage);
+
+  const prevButtonDisabled = currentPage === 1 ? "pagination__link--disabled" : "";
+  paginationContainer.innerHTML += `
+        <li class="pagination__item">
+            <a class="pagination__link ${prevButtonDisabled}" href="#" data-page="previous">Anterior</a>
+        </li>
+    `;
+
+  pages.forEach((p) => {
+    if (p === "...") {
+      paginationContainer.innerHTML += `<li><span class="pagination__ellipsis">...</span></li>`;
+    } else {
+      const isActive = p === currentPage ? "pagination__link--active" : "";
+      const ariaCurrent = p === currentPage ? 'aria-current="page"' : "";
+      paginationContainer.innerHTML += `
+                <li class="pagination__item">
+                    <a class="pagination__link ${isActive}" ${ariaCurrent} href="#" data-page="${p}">${p}</a>
+                </li>
+            `;
+    }
+  });
+
+  const nextButtonDisabled =
+    currentPage === totalPages ? "pagination__link--disabled" : "";
+  paginationContainer.innerHTML += `
+        <li class="pagination__item">
+            <a class="pagination__link ${nextButtonDisabled}" href="#" data-page="next">Próximo</a>
+        </li>
+    `;
+}
+
+function createPaginationLogic(totalPages, currentPage) {
+  const pagesToShow = [];
+  const pagesAround = 1;
+
+  //Se nao tiver muitas paginas de resultado, isso vai mostrar todas
+  if (totalPages <= 5) {
+    for (let i = 1; i <= totalPages; i++) {
+      pagesToShow.push(i);
+    }
+    return pagesToShow;
+  }
+
+  //Se tiver muitas paginas irá seguir a lógica de
+  //[primeira pagina, ..., x, x+1, x+2 ... ultima pagina ]
+
+  //Primeira página sempre obrigatória
+  pagesToShow.push(1);
+
+  //Se a pagina atual tiver mais de 3 casas de distâncias do inicio, então simule reticências
+  if (currentPage > pagesAround + 2) {
+    pagesToShow.push("...");
+  }
+
+  // Lógica para mostrar -> [pagina anterior, atual, próxima] -> Com exceção da primeira e última página que sempre serão mostradas
+  
+  for (
+    let i = Math.max(2, currentPage - pagesAround);
+    i <= Math.min(totalPages - 1, currentPage + pagesAround);
+    i++
+  ) {
+    if (!pagesToShow.includes(i)) {
+      pagesToShow.push(i);
+    }
+  }
+
+  //Se a pagina atual tiver mais de 3 casas de distâncias do final, então simule reticências
+  if (currentPage < totalPages - (pagesAround + 1)) {
+    pagesToShow.push("...");
+  }
+
+  //última página sempre obrigatória
+  if (!pagesToShow.includes(totalPages)) {
+    pagesToShow.push(totalPages);
+  }
+
+  return pagesToShow;
+}
+
+function handlePaginationClick(event){
+
+  event.preventDefault();
+  const clickedElement = event.target;
+
+  if (
+    clickedElement.tagName !== "A" ||
+    clickedElement.classList.contains("pagination__link--disabled")
+  ) {
+    return;
+  }
+
+  let { totalItems, itemsPerPage, currentPage } = paginationState;
+  const totalPages = Math.ceil(totalItems / itemsPerPage);
+
+  let newPage = currentPage;
+
+  const pageToGo = clickedElement.dataset.page;
+
+  if (pageToGo === "previous") {
+    if (currentPage > 1) {
+      newPage--;
+    }
+  } 
+  else if (pageToGo === "next") {
+    if (currentPage < totalPages) {
+      newPage++;
+    }
+  } else {
+    newPage = parseInt(pageToGo, 10); 
+  }
+
+  //Atualiza o estado global
+  paginationState.currentPage = newPage;
+
+  handleInitialPageFetch();
+}
+
+
+
+//===================================EXECUÇÃO=============================================================
 function startPage() {
   handleInitialPageFetch();
   handleFormSubmit();
+
+  paginationContainer.addEventListener("click", handlePaginationClick);
 }
+
+
+
 
 startPage();
